c19d431eecc86a7715d67423a240035464e5c5ba
Miles Bardon <miles.bardon@tesco.com>
2021-01-29 13:10:35 +0000

    Using fetch instead of app

--- GithubApp/index.ts
+++ GithubApp/index.ts
@@ -1,5 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { App } from "@slack/bolt";
+import fetch from "node-fetch";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -8,38 +9,40 @@ const httpTrigger: AzureFunction = async function (
   context.log("Headers:", req.headers);
   context.log("Body:", req.body);
 
-  // await fetch("https://slack.com/api/chat.postMessage", {
-  //   method: "POST",
-  //   headers: {
-  //     Authorization: process.env.SLACK_BOT_TOKEN,
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     channel: "merge",
-  //     icon_emoji: ":steam_locomotive:",
-  //     text: "I have been sent from GitHub, be not afraid.",
-  //   }),
-  // });
+  await fetch("https://slack.com/api/chat.postMessage", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      channel: "C015KE3RXGB",
+      icon_emoji: ":steam_locomotive:",
+      text: "I have been sent from GitHub, be not afraid.",
+    }),
+  });
 
   const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
   });
 
-  const channelName = "merge";
-  // const channelId = await app.client.conversations
-  //   .list()
-  //   .then((res) => res.channels)
-  //   .then(
-  //     (channels: [{ id: string; name: string }]) =>
-  //       channels.find(({ name }) => name === channelName).id
-  //   );
+  context.log(app.client.conversations.list());
 
-  app.client.chat.postMessage({
-    channel: "C015KE3RXGB",
-    icon_emoji: ":steam_locomotive:",
-    text: "I have been sent from GitHub, be not afraid.",
-  });
+  // const channelName = "merge";
+  // // const channelId = await app.client.conversations
+  // //   .list()
+  // //   .then((res) => res.channels)
+  // //   .then(
+  // //     (channels: [{ id: string; name: string }]) =>
+  // //       channels.find(({ name }) => name === channelName).id
+  // //   );
+
+  // app.client.chat.postMessage({
+  //   channel: "C015KE3RXGB",
+  //   icon_emoji: ":steam_locomotive:",
+  //   text: "I have been sent from GitHub, be not afraid.",
+  // });
 };
 
 export default httpTrigger;
