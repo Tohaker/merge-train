383eef869e8bccdfecc997c3d820597ec488b4ec
Miles Bardon <miles.bardon@tesco.com>
2021-01-29 12:10:29 +0000

    Using hardcoded channel ID

--- GithubApp/index.ts
+++ GithubApp/index.ts
@@ -27,16 +27,16 @@ const httpTrigger: AzureFunction = async function (
   });
 
   const channelName = "merge";
-  const channelId = await app.client.conversations
-    .list()
-    .then((res) => res.channels)
-    .then(
-      (channels: [{ id: string; name: string }]) =>
-        channels.find(({ name }) => name === channelName).id
-    );
+  // const channelId = await app.client.conversations
+  //   .list()
+  //   .then((res) => res.channels)
+  //   .then(
+  //     (channels: [{ id: string; name: string }]) =>
+  //       channels.find(({ name }) => name === channelName).id
+  //   );
 
   app.client.chat.postMessage({
-    channel: channelId,
+    channel: "C015KE3RXGB",
     icon_emoji: ":steam_locomotive:",
     text: "I have been sent from GitHub, be not afraid.",
   });
