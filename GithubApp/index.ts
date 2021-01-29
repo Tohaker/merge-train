2d6e1b4c804aea6e635223dba348391cf8e13725
Miles Bardon <miles.bardon@tesco.com>
2021-01-29 15:36:42 +0000

    Adding logic to remove from list

--- GithubApp/index.ts
+++ GithubApp/index.ts
@@ -1,12 +1,18 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import crypto from "crypto";
 import fetch from "node-fetch";
-import { connectToCosmos, createItem, readAllItems } from "../common/cosmos";
+import {
+  connectToCosmos,
+  createItem,
+  deleteItem,
+  readAllItems,
+} from "../common/cosmos";
 
 type PullRequest = {
   html_url: string;
   title: string;
   created_at: string;
+  updated_at: string;
 };
 
 type Label = {
@@ -45,61 +51,122 @@ const httpTrigger: AzureFunction = async function (
     sender: Sender;
   } = req.body;
 
-  if (action === "labeled" && label.name.toLowerCase().includes("merge")) {
-    const blocks = [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `A new PR is ready to merge:\n*<${pull_request.html_url}|${pull_request.title}>*`,
-        },
-      },
-      {
-        type: "section",
-        fields: [
-          {
+  if (label.name.toLowerCase().includes("merge")) {
+    if (action === "labeled") {
+      const blocks = [
+        {
+          type: "section",
+          text: {
             type: "mrkdwn",
-            text: `*Opened by:*\n${sender.login}`,
+            text: `A new PR is ready to merge:\n*<${pull_request.html_url}|${pull_request.title}>*`,
           },
-          {
-            type: "mrkdwn",
-            text: `*When:*\n${new Date(
-              pull_request.created_at
-            ).toDateString()}`,
-          },
-        ],
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: "This has now been added to the list :page_with_curl:",
         },
-      },
-    ];
+        {
+          type: "section",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*Changed by:*\n${sender.login}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*When:*\n${new Date(
+                pull_request.created_at
+              ).toDateString()}`,
+            },
+          ],
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "This has now been added to the list :page_with_curl:",
+          },
+        },
+      ];
 
-    const container = connectToCosmos();
-    const items = await readAllItems(container);
+      const container = connectToCosmos();
+      const items = await readAllItems(container);
 
-    if (items.some(({ url }) => url === pull_request.html_url)) {
-      context.log.info(`PR (${pull_request.html_url}) already saved`);
-      return;
-    } else {
+      if (items.some(({ url }) => url === pull_request.html_url)) {
+        context.log.info(`PR (${pull_request.html_url}) already saved`);
+        return;
+      } else {
+        try {
+          await createItem(container, pull_request.html_url);
+
+          await fetch("https://slack.com/api/chat.postMessage", {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              channel: "C015KE3RXGB",
+              icon_emoji: ":steam_locomotive:",
+              blocks,
+            }),
+          });
+        } catch (e) {
+          context.log.warn("Error creating item: ", e);
+        }
+      }
+    } else if (action === "unlabled") {
+      const blocks = [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `A PR has had its status changed:\n*<${pull_request.html_url}|${pull_request.title}>*`,
+          },
+        },
+        {
+          type: "section",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*Changed by:*\n${sender.login}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*When:*\n${new Date(
+                pull_request.updated_at
+              ).toDateString()}`,
+            },
+          ],
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "This has now been removed to the list :page_with_curl:",
+          },
+        },
+      ];
+
+      const container = connectToCosmos();
+      const items = await readAllItems(container);
+
       try {
-        await createItem(container, pull_request.html_url);
+        const id = items.find(({ url }) => url === pull_request.html_url)?.id;
+        if (id) {
+          await deleteItem(container, id);
 
-        await fetch("https://slack.com/api/chat.postMessage", {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            channel: "C015KE3RXGB",
-            icon_emoji: ":steam_locomotive:",
-            blocks,
-          }),
-        });
+          await fetch("https://slack.com/api/chat.postMessage", {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              channel: "C015KE3RXGB",
+              icon_emoji: ":steam_locomotive:",
+              blocks,
+            }),
+          });
+        } else {
+          context.log.warn("No ID found for this url");
+        }
       } catch (e) {
         context.log.warn("Error creating item: ", e);
       }
