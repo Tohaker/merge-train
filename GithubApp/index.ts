5a253f36bad9c8f801a5e284afbb5d3506d922b5
Miles Bardon <miles.bardon@tesco.com>
2021-01-29 14:09:54 +0000

    Setting slack response

--- GithubApp/index.ts
+++ GithubApp/index.ts
@@ -1,48 +1,102 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import { App } from "@slack/bolt";
+import crypto from "crypto";
 import fetch from "node-fetch";
+
+type PullRequest = {
+  html_url: string;
+  title: string;
+  created_at: string;
+};
+
+type Label = {
+  name: string;
+};
+
+type Sender = {
+  login: string;
+  avatar_url: string;
+};
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  context.log("Headers:", req.headers);
   context.log("Body:", req.body);
 
-  await fetch("https://slack.com/api/chat.postMessage", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      channel: "C015KE3RXGB",
-      icon_emoji: ":steam_locomotive:",
-      text: "I have been sent from GitHub, be not afraid.",
-    }),
-  });
+  const receivedSignature = req.headers["x-hub-signature"].split("=");
+  const calculatedSignature = crypto
+    .createHmac(receivedSignature[0], receivedSignature[1])
+    .update(req.body)
+    .digest("hex");
+  const calculatedSignatureRaw = crypto
+    .createHmac(receivedSignature[0], receivedSignature[1])
+    .update(req.rawBody)
+    .digest("hex");
 
-  const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-  });
+  console.log("received signature:", receivedSignature[1]);
+  console.log("calc:", calculatedSignature);
+  console.log("calcRaw:", calculatedSignatureRaw);
 
-  context.log(app.client.conversations.list());
+  const {
+    action,
+    pull_request,
+    label,
+    sender,
+  }: {
+    action: string;
+    pull_request: PullRequest;
+    label: Label;
+    sender: Sender;
+  } = req.body;
 
-  // const channelName = "merge";
-  // // const channelId = await app.client.conversations
-  // //   .list()
-  // //   .then((res) => res.channels)
-  // //   .then(
-  // //     (channels: [{ id: string; name: string }]) =>
-  // //       channels.find(({ name }) => name === channelName).id
-  // //   );
+  if (action === "labeled") {
+    const blocks = {
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `A new PR is ready to merge:\n*<${pull_request.html_url}|${pull_request.title}>*`,
+          },
+        },
+        {
+          type: "section",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*Opened by:*\n${sender.login}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*When:*\n${new Date(
+                pull_request.created_at
+              ).toDateString()}`,
+            },
+          ],
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "This has now been added to the list :page_with_curl:",
+          },
+        },
+      ],
+    };
 
-  // app.client.chat.postMessage({
-  //   channel: "C015KE3RXGB",
-  //   icon_emoji: ":steam_locomotive:",
-  //   text: "I have been sent from GitHub, be not afraid.",
-  // });
+    fetch("https://slack.com/api/chat.postMessage", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        channel: "C015KE3RXGB",
+        icon_url: sender.avatar_url,
+        blocks,
+      }),
+    });
+  }
 };
 
 export default httpTrigger;
