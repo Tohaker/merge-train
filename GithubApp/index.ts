2b28c59403e72200e62a4e2b2877252f1ade5a22
Miles Bardon <miles.bardon@tesco.com>
2021-01-29 12:03:27 +0000

    Getting channel ID dynamically

--- GithubApp/index.ts
+++ GithubApp/index.ts
@@ -8,13 +8,35 @@ const httpTrigger: AzureFunction = async function (
   context.log("Headers:", req.headers);
   context.log("Body:", req.body);
 
+  // await fetch("https://slack.com/api/chat.postMessage", {
+  //   method: "POST",
+  //   headers: {
+  //     Authorization: process.env.SLACK_BOT_TOKEN,
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     channel: "merge",
+  //     icon_emoji: ":steam_locomotive:",
+  //     text: "I have been sent from GitHub, be not afraid.",
+  //   }),
+  // });
+
   const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
   });
 
+  const channelName = "merge";
+  const channelId = await app.client.conversations
+    .list()
+    .then((res) => res.channels)
+    .then(
+      (channels: [{ id: string; name: string }]) =>
+        channels.find(({ name }) => name === channelName).id
+    );
+
   app.client.chat.postMessage({
-    channel: "merge",
+    channel: channelId,
     icon_emoji: ":steam_locomotive:",
     text: "I have been sent from GitHub, be not afraid.",
   });
