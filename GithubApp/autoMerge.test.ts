7f31ad0ba8d2d6cb3108f98a5c578309fccaf865
Miles Bardon <milesbardon@gmail.com>
2021-02-26 01:22:09 +0000

    Moving status handler to autoMerge

--- GithubApp/autoMerge.test.ts
+++ GithubApp/autoMerge.test.ts
@@ -1,23 +1,30 @@
 import { WebClient } from "@slack/web-api";
-import { PullRequest } from "@octokit/webhooks-definitions/schema";
+import { PullRequest, StatusEvent } from "@octokit/webhooks-definitions/schema";
+import { getItems, getMergeableItems } from "../graphql/queue";
+import { handleItemAdded, handleStateReported } from "./autoMerge";
+import { Context } from "@azure/functions";
 
-describe("Auto Merge", () => {
-  let handleItemAdded: (
-    client: WebClient,
-    pr: PullRequest,
-    channel: string
-  ) => Promise<void>;
+jest.mock("../graphql/queue");
+jest.mock("../common/config", () => ({
+  Branch: {
+    DEFAULT: "master",
+  },
+  icon_emoji: "emoji",
+}));
 
-  const mockGetQueue = jest.fn();
-  const mockGetItems = jest.fn();
+const mockGetItems = getItems as jest.MockedFunction<typeof getItems>;
+const mockGetMergeableItems = getMergeableItems as jest.MockedFunction<
+  typeof getMergeableItems
+>;
 
-  const mockWebClient: WebClient = {
-    //@ts-ignore
-    chat: {
-      postMessage: jest.fn(),
-    },
-  };
+const mockWebClient: WebClient = {
+  //@ts-ignore
+  chat: {
+    postMessage: jest.fn(),
+  },
+};
 
+describe("handleItemAdded", () => {
   //@ts-ignore
   const mockPR: PullRequest = {
     url: "mockUrl",
@@ -28,31 +35,28 @@ describe("Auto Merge", () => {
     mergeable: true,
   };
 
-  beforeEach(() => {
-    mockGetQueue.mockResolvedValue({ data: true });
-
-    jest.mock("../graphql/queue", () => ({
-      getQueue: mockGetQueue,
-      getItems: mockGetItems,
-    }));
-
-    handleItemAdded = require("./autoMerge").handleItemAdded;
-  });
+  const mockContext: Context = {
+    //@ts-ignore
+    log: jest.fn(),
+  };
 
   describe("given the queue has 2 items", () => {
     beforeEach(() => {
-      mockGetItems.mockReturnValue([{ mergeable: true }, { mergeable: true }]);
+      //@ts-ignore
+      mockGetItems.mockReturnValue([mockPR, mockPR]);
     });
 
     it("should not post any message", async () => {
-      await handleItemAdded(mockWebClient, mockPR, "channel");
+      await handleItemAdded(mockWebClient, mockPR, "channel", mockContext);
       expect(mockWebClient.chat.postMessage).not.toBeCalled();
+      expect(mockContext.log).toBeCalled();
     });
   });
 
   describe("given the queue has 1 item", () => {
     beforeEach(() => {
-      mockGetItems.mockReturnValue([{ mergeable: true }]);
+      //@ts-ignore
+      mockGetItems.mockReturnValue([mockPR]);
     });
 
     describe("given the pr is not mergeable", () => {
@@ -61,9 +65,9 @@ describe("Auto Merge", () => {
       });
 
       it("should post a message", async () => {
-        await handleItemAdded(mockWebClient, mockPR, "channel");
+        await handleItemAdded(mockWebClient, mockPR, "channel", mockContext);
         expect(mockWebClient.chat.postMessage).toBeCalledWith({
-          icon_emoji: ":steam_locomotive:",
+          icon_emoji: "emoji",
           text:
             "This PR cannot be merged yet, remove the label until this is resolved.",
           channel: "channel",
@@ -77,12 +81,96 @@ describe("Auto Merge", () => {
       });
 
       it("should post a message", async () => {
-        await handleItemAdded(mockWebClient, mockPR, "channel");
+        await handleItemAdded(mockWebClient, mockPR, "channel", mockContext);
         expect(mockWebClient.chat.postMessage).toBeCalledWith({
-          icon_emoji: ":steam_locomotive:",
+          icon_emoji: "emoji",
           text: "I would have merged <mockUrl|PR> now, is it a good time?",
           channel: "channel",
         });
+      });
+    });
+  });
+});
+
+describe("handleStateReported", () => {
+  let mockBody: StatusEvent;
+
+  beforeEach(() => {
+    //@ts-ignore
+    mockBody = {
+      branches: [
+        {
+          name: "master",
+          commit: { sha: "1234", url: "url1" },
+          protected: true,
+        },
+        {
+          name: "some-branch",
+          commit: { sha: "1357", url: "url2" },
+          protected: false,
+        },
+      ],
+      sha: "1234",
+    };
+  });
+
+  describe("given the matching branch is not the default branch", () => {
+    beforeEach(() => {
+      mockBody.sha = "1357";
+    });
+
+    it("should do nothing", async () => {
+      await handleStateReported(mockWebClient, mockBody, "1234");
+
+      expect(mockGetMergeableItems).not.toBeCalled();
+      expect(mockWebClient.chat.postMessage).not.toBeCalled();
+    });
+  });
+
+  describe("given the matching branch is the default branch", () => {
+    describe("given there are mergeable items", () => {
+      beforeEach(() => {
+        mockGetMergeableItems.mockReturnValue([
+          //@ts-ignore
+          {
+            url: "https://some.url",
+            title: "title",
+          },
+          //@ts-ignore
+          {
+            url: "https://some2.url",
+            title: "title2",
+          },
+        ]);
+      });
+
+      it("should post a message", async () => {
+        await handleStateReported(mockWebClient, mockBody, "1234");
+
+        expect(mockWebClient.chat.postMessage).toBeCalledWith({
+          icon_emoji: "emoji",
+          text: `<https://some.url|title> would have been merged now. Is it a good time?`,
+          channel: "1234",
+        });
+        expect(mockWebClient.chat.postMessage).toBeCalledTimes(1);
+      });
+    });
+
+    describe("given there are no mergeable items", () => {
+      beforeEach(() => {
+        mockGetMergeableItems.mockReturnValue([]);
+      });
+
+      it("should post a message", async () => {
+        await handleStateReported(mockWebClient, mockBody, "1234");
+
+        expect(mockWebClient.chat.postMessage).toBeCalledWith({
+          icon_emoji: "emoji",
+          text:
+            "The last merge was successful, but no PRs are ready to be merged.\nCheck the list and manually merge to start again.",
+          channel: "1234",
+        });
+        expect(mockWebClient.chat.postMessage).toBeCalledTimes(1);
       });
     });
   });
--- GithubApp/autoMerge.ts
+++ GithubApp/autoMerge.ts
@@ -1,32 +1,67 @@
+import { Context } from "@azure/functions";
 import { WebClient } from "@slack/web-api";
-import { PullRequest } from "@octokit/webhooks-definitions/schema";
-import { getQueue, getItems } from "../graphql/queue";
+import { PullRequest, StatusEvent } from "@octokit/webhooks-definitions/schema";
+import { getQueue, getItems, getMergeableItems } from "../graphql/queue";
+import { Branch, icon_emoji } from "../common/config";
 
 export const handleItemAdded = async (
   client: WebClient,
   pullRequest: PullRequest,
-  channel: string
+  channel: string,
+  context: Context
 ) => {
   const queue = await getQueue();
 
   if (getItems(queue).length > 1) {
-    console.log("Queue was not empty, no new merges to perform");
+    context.log("Queue was not empty, no new merges to perform");
     return;
   }
 
   if (pullRequest.mergeable) {
     // TODO: Replace with actual merge request
     await client.chat.postMessage({
-      icon_emoji: ":steam_locomotive:",
+      icon_emoji,
       text: `I would have merged <${pullRequest.url}|${pullRequest.title}> now, is it a good time?`,
       channel,
     });
   } else {
     await client.chat.postMessage({
-      icon_emoji: ":steam_locomotive:",
+      icon_emoji,
       text:
         "This PR cannot be merged yet, remove the label until this is resolved.",
       channel,
     });
+  }
+};
+
+export const handleStateReported = async (
+  client: WebClient,
+  body: StatusEvent,
+  channel: string
+) => {
+  const { branches, sha } = body;
+  const matchingBranch = branches.find(({ commit }) => commit.sha === sha);
+
+  // Merges should only happen on the default branch, which can be customised.
+  if (matchingBranch?.name === Branch.DEFAULT) {
+    const queue = await getQueue();
+    const mergeableItems = getMergeableItems(queue);
+
+    if (mergeableItems.length) {
+      const prToMerge = mergeableItems.shift();
+      // TODO: Replace with actual merge request
+      await client.chat.postMessage({
+        icon_emoji,
+        text: `<${prToMerge.url}|${prToMerge.title}> would have been merged now. Is it a good time?`,
+        channel,
+      });
+    } else {
+      await client.chat.postMessage({
+        icon_emoji,
+        text:
+          "The last merge was successful, but no PRs are ready to be merged.\nCheck the list and manually merge to start again.",
+        channel,
+      });
+    }
   }
 };
